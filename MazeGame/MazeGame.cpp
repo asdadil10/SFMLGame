d1e21309f35d18828f07ad3f7b5fe6f46b93b8d7
HP <HP@DESKTOP-790S9AQ>
2026-02-08 17:08:25 +0500

    Add project files.

---
+++ MazeGame/MazeGame.cpp
@@ -0,0 +1,154 @@
+﻿#include "MazeGame.h"
+
+void updateFps(sf::Text *fps,double dt) {
+    fps->setString(std::to_string(int(1/dt)));
+}
+
+int main()
+{
+    sf::ContextSettings contextSettings;
+    contextSettings.antiAliasingLevel = 8;
+    // Main Window
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u{1280,720}), "Maze", sf::State::Fullscreen,contextSettings);
+    window.setFramerateLimit(60);
+    window.setMouseCursorVisible(false);
+    window.setVerticalSyncEnabled(true);
+    // Bg image
+    sf::Image tile("src/grass.bmp");
+    upscale(&tile); // 4x upscaling
+    tile = *createTiles({ 1280,720 }, tile.getPixelsPtr());
+    const sf::Texture texture(tile);
+    sf::Sprite sprite(texture);
+    //delete createTiles({800,800},pixels);
+
+    // Font
+    const sf::Font font("C:/Users/HP/Downloads/Helvetica.ttf");
+
+    sf::Text fps(font, "", 44);
+    fps.setFillColor({ 255,0,0,255 });
+    
+    //sf::Music music("C:/Users/HP/Downloads/Aura - Shubh.mp3");
+    //music.play();
+    
+    // Player
+    sf::RectangleShape player({ 32,32 });
+    sf::Image pImage("src/player.png");
+    upscale(&pImage);
+    sf::Texture pTex(pImage);
+    player.setTexture(&pTex);
+    player.setOrigin({ 16,16 });
+    sf::Vector2f playerPos = { 0,0 };
+    sf::Vector2i mousePos;
+    bool playerAlive = true;
+
+    // Projectile
+    std::vector<Projectile> bullets;
+    double dt;
+
+    // Enemy
+    Enemy enemy;
+    bool enemyAlive = true;
+    //bool playerMoved = false;
+    while (window.isOpen())
+    {
+        // get delta time
+        dt = deltaT();
+
+        // update fps
+        static float fpsUpdateThreshold;
+        fpsUpdateThreshold += dt;
+        if (fpsUpdateThreshold > (1 / 10.0)) {
+            updateFps(&fps, dt);
+            fpsUpdateThreshold = 0;
+        }
+
+        // Process events
+        static double holdTime = 0;
+        holdTime += dt;
+
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LAlt) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RAlt)) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::F4)) {
+                window.close();
+            }
+
+            /*if (event->is<sf::Event::KeyPressed>()) {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {playerPos += {0 ,-1}; playerMoved=true;}
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {playerPos += {-1, 0}; playerMoved=true;}
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {playerPos += {0 , 1} ; playerMoved=true;}
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {playerPos += {1 , 0} ; playerMoved=true;}
+            }*/
+        }
+        /*if (playerMoved) {
+            player.setPosition(playerPos);
+            playerMoved = false;
+        }
+        */
+        if (playerAlive)
+        {
+            // Move player with mouse
+            if (sf::Mouse::getPosition() - window.getPosition() != mousePos) {
+                mousePos = sf::Mouse::getPosition() - window.getPosition();
+                player.setPosition(sf::Vector2f(mousePos));
+                if(enemyAlive)
+                    if (enemy.checkCollision(sf::Vector2f(mousePos))) {
+                        playerAlive = false;
+                        GameOver(&window, false);
+                    }
+            }
+
+            // handle Bullets
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                if (holdTime > (1 / 10.0)) {
+                    Projectile* bullet = new Projectile;
+                    bullet->bullet.setPosition(sf::Vector2f(mousePos)+sf::Vector2f{0.f,-16.f});
+                    bullets.push_back(*bullet);
+                    delete bullet;
+                    holdTime = 0;
+                }
+            }
+        }
+        // Update Enemy
+        if (enemyAlive)
+            enemy.update(dt);
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+        
+        // Draw player
+        if (playerAlive)
+            window.draw(player);
+        
+        // Enemy
+        if (enemyAlive)
+            window.draw(enemy.enemy);
+
+        // Bulllets
+        for (int i = 0; i < bullets.size();i++) {
+            bullets[i].update(dt);
+            sf::Vector2f pos = bullets[i].bullet.getPosition();
+            if (enemyAlive)
+                if (enemy.checkCollision(pos))
+                    enemyAlive = false;
+            window.draw(bullets[i].bullet);
+            if (pos.y < 0) {
+                bullets.erase(bullets.begin() + i);
+            }
+        }
+
+        if (!enemyAlive)
+            GameOver(&window, true);
+        else if (!playerAlive)
+            GameOver(&window, false);
+        // Fps
+        window.draw(fps);
+
+        // Update the window
+        window.display();
+    }
+}
---
+++ MazeGame/MazeGame.h
@@ -0,0 +1,155 @@
+﻿// MazeGame.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <vector>
+
+struct Enemy {
+    sf::CircleShape enemy;
+    bool movingLeft = true;
+    Enemy(){
+        enemy.setFillColor(sf::Color::Red);
+        enemy.setPosition({500,100});
+        enemy.setRadius(32);
+    }
+    void update(double dt) {
+        sf::Vector2f pos = enemy.getPosition();
+        if (0 < pos.x && pos.x < 80) movingLeft = true;
+        if (1200 < pos.x && pos.x < 1280) movingLeft = false;
+        if (movingLeft) {
+            enemy.setPosition(pos + sf::Vector2f({ float_t(255 * dt),0 }));
+            enemy.setFillColor({ 255,128,0 });
+        }
+        else {
+            enemy.setPosition(pos + sf::Vector2f({ -float_t(255 * dt),0 }));
+            enemy.setFillColor({ 255,0,128 });
+        }
+    }
+    bool checkCollision(sf::Vector2f pos) {
+        if (enemy.getGlobalBounds().contains(pos))
+            return true;
+        else 
+            return false;
+    }
+};
+
+struct Projectile {
+    sf::CircleShape bullet;
+    Projectile() {
+        bullet.setRadius(6);
+        bullet.setPointCount(9);
+        bullet.setFillColor(sf::Color{ 164,64,64,255 });
+        bullet.setOutlineThickness(2);
+        bullet.setOutlineColor(sf::Color{ 128,32,64,196 });
+        bullet.setOrigin({6, 6});
+    }
+    void update(double dt) {
+        bullet.setPosition(bullet.getPosition() + sf::Vector2f({ 0,-float_t(555*dt) }));
+    }
+};
+
+void upscale(sf::Image* image)
+{
+    sf::Color pixels[32][32];
+    uint8_t* p = new uint8_t[32 * 32 * 4];
+    int px= 0;
+    for (uint16_t i = 0; i < 8; i++) // y loop
+    {
+        for (uint8_t y = 0; y < 4; y++) // for repeating y-pixels 4 times
+        {
+            for (uint16_t j = 0; j < 8; j++) // x loop
+            {
+                for (uint8_t x = 0; x < 4; x++) // for repeating x-pixels 4 times
+                {
+                    pixels[j * 4 + x][i * 4 + x] = image->getPixel(sf::Vector2u{ j,i });
+                    p[px] = pixels[j * 4 + x][i * 4 + x].r; px++;
+                    p[px] = pixels[j * 4 + x][i * 4 + x].g; px++;
+                    p[px] = pixels[j * 4 + x][i * 4 + x].b; px++;
+                    p[px] = pixels[j * 4 + x][i * 4 + x].a; px++;
+                }
+            }
+        }
+    }
+    image->resize({ 32,32 }, p);
+    delete[] p;
+}
+sf::Color** pixelToColorArr(sf::Vector2u dimensions, const uint8_t* pixels)
+{
+    sf::Color** cPixels = new sf::Color * [dimensions.y];
+    for (int i = 0; i < dimensions.y; i++) {
+        cPixels[i] = new sf::Color[dimensions.x];
+    }
+    if (pixels == nullptr)
+        return cPixels;
+    int count = 0;
+    for (int i = 0; i < dimensions.y; i++) {
+        for (int j = 0; j < dimensions.x; j++) {
+            cPixels[i][j].r = pixels[count]; count++;
+            cPixels[i][j].g = pixels[count]; count++;
+            cPixels[i][j].b = pixels[count]; count++;
+            cPixels[i][j].a = pixels[count]; count++;
+        }
+    }
+    return cPixels;
+}
+
+uint8_t* colorToPixelArr(sf::Vector2u dimensions, sf::Color** cPixels)
+{
+    uint8_t* pixels = new uint8_t[dimensions.x * dimensions.y * 4];
+    unsigned int count = 0;
+    for (int i = 0; i < dimensions.y; i++) {
+        for (int j = 0; j < dimensions.x; j++) {
+            pixels[count] = cPixels[i][j].r; count++;
+            pixels[count] = cPixels[i][j].g; count++;
+            pixels[count] = cPixels[i][j].b; count++;
+            pixels[count] = cPixels[i][j].a; count++;
+        }
+    }
+    return pixels;
+}
+sf::Image* createTiles(const sf::Vector2u dimensions, const uint8_t* pixelData) //32x32 tile
+{
+    sf::Image* image = new sf::Image;
+    sf::Color** Cx = pixelToColorArr(dimensions, nullptr);
+    double tileX, tileY;
+    tileY = dimensions.y / 32;
+    tileX = dimensions.x / 32;
+    sf::Color** cPixels = pixelToColorArr({ 32,32 }, pixelData);
+    for (int i = 0; i < dimensions.y; i++) {
+        for (int j = 0; j < dimensions.x; j++) {
+            Cx[i][j] = cPixels[i % 32][j % 32];
+        }
+    }
+    uint8_t* p = colorToPixelArr(dimensions, Cx);
+    
+    for (int i = 0; i < dimensions.y; i++) { delete[] Cx[i]; } delete[] Cx;
+    for (int i = 0; i < 32; i++) { delete[]cPixels[i]; } delete[]cPixels;
+
+    image->resize(dimensions, p);
+    return image;
+}
+
+double deltaT()
+{
+    static sf::Clock clock;
+    double dt = clock.getElapsedTime().asSeconds();
+    clock.restart();
+    return dt;
+}
+
+void GameOver(sf::RenderWindow * window,bool win)
+{
+    const sf::Font font("C:/Users/HP/Downloads/Helvetica.ttf");
+    sf::Text text(font, "You Won!", 96);
+    if (!win) text.setString("You Lost!");
+    text.setOrigin(text.getGlobalBounds().getCenter());
+    text.setPosition({ (float_t)(window->getSize().x / 2),(float_t)(window->getSize().y / 2) });
+    window->draw(text);
+}
